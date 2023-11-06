3cbe80923ee8c3f0ba951a650d5095a021c37dcd
z1mer-off <artt.art.14@mail.ru>
2023-11-06 17:12:29 +0300

    added comments

--- lab02/hello_world.cpp
+++ lab02/hello_world.cpp
@@ -1,7 +1,7 @@
 #include <iostream>
 int main(int argc,char** argv)
 {
-	string a;
+	string a; //переменная для хранения имени пользователя
 	std::cout<<"Введите имя пользователя: ";
 	std::cin>>a;
 	std::cout<<"\nHello world from "<<a<<endl;
